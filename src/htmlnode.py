fee8f8ba3479a2e715637ae736951dbfd089d355
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-08-31 09:59:18 +1000

    add src/htmlnode.py
    - add HTMLNode and LeafNode classes

---
+++ src/htmlnode.py
@@ -0,0 +1,31 @@
+class HTMLNode():
+    def __init__(self, tag = None, val = None, children = None, props = None):
+        self.tag = tag
+        self.value = val
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        result = ""
+        for prop in self.props:
+            result += f' {prop}="{self.props[prop]}"'
+        return result
+    
+    def __repr__(self):
+        return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, val, props=None):
+        super().__init__(tag, val, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode has no value")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
