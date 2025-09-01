50c55c41be14c1ebf44ddbe8fa786eaf8af222f9
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-01 11:43:14 +1000

    update src/test_split_nodes.py
    - add unit testing for split nodes image and link

--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,6 +1,6 @@
 import unittest
 
-from split_nodes import split_nodes_delimiter
+from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextNodeType
 
 
@@ -34,6 +34,42 @@ class TestSplitNodes(unittest.TestCase):
             TextNode(" node", TextNodeType.TEXT)
         ]
         self.assertEqual(nodes, test_nodes)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextNodeType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextNodeType.TEXT),
+                TextNode("image", TextNodeType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextNodeType.TEXT),
+                TextNode(
+                    "second image", TextNodeType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextNodeType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextNodeType.TEXT),
+                TextNode("to boot dev", TextNodeType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextNodeType.TEXT),
+                TextNode(
+                    "to youtube", TextNodeType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes
+        )
 
 
 if __name__ == "__main__":
