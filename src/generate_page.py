450435f8d93f648acbee21c6527e011fcefe4fa5
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-06 08:45:45 +1000

    add src/generate_page.py
    - add generate_page function

---
+++ src/generate_page.py
@@ -0,0 +1,19 @@
+import os
+
+from extract_title import extract_title
+from markdown_to_html_node import markdown_to_html_node
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as file:
+        markdown = file.read()
+    with open(template_path) as file:
+        template = file.read()
+    html_string = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    template.replace("{{ Title }}", title)
+    template.replace("{{ Content }}", html_string)
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+    with open(dest_path, "w") as file:
+        file.write(template)
