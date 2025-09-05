7212ed1824da8eee214980ed72c2765c419a8841
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-06 09:20:42 +1000

    update src/generate_page.py
    - fix template replacement bug

--- src/generate_page.py
+++ src/generate_page.py
@@ -12,8 +12,8 @@ def generate_page(from_path, template_path, dest_path):
         template = file.read()
     html_string = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    template.replace("{{ Title }}", title)
-    template.replace("{{ Content }}", html_string)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html_string)
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, "w") as file:
