4fac8dced5f0aee4d8666b742437d8df19b467d0
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-06 09:18:44 +1000

    update src/test_markdown_to_html_node.py
    - add unit tests for ordered and unordered lists

--- src/test_markdown_to_html_node.py
+++ src/test_markdown_to_html_node.py
@@ -35,6 +35,30 @@ the **same** even with inline stuff
             html,
             "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff</code></pre></div>",
         )
+    
+    def test_unorderedlist_block(self):
+        md = """
+- You can spend years studying the legendarium and still not understand its depths
+- It can be enjoyed by children and adults alike
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>You can spend years studying the legendarium and still not understand its depths</li><li>It can be enjoyed by children and adults alike</li></ul></div>"
+        )
+
+    def test_orderedlist_block(self):
+        md = """
+1. Gandalf
+2. Bilbo
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>Gandalf</li><li>Bilbo</li></ol></div>"
+        )
 
 
 if __name__ == "__main__":
