fad62b369e3e5682564ce9dda3e312a826a21ea0
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-09-06 08:46:56 +1000

    update src/main.py
    - update to generate page from content/index.md and template.html

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import os
 import shutil
 
+from generate_page import generate_page
+
 def main():
     source = './static'
     dest = './public'
     if os.path.exists(dest):
         shutil.rmtree(dest)
     shutil.copytree(source, dest)
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 
 if __name__ == "__main__":
